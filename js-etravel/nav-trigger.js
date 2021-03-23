ddea4b2a05eb53cbb38adf58655a3e930a390064
Plonk-jpeg <75024242+Plonk-jpeg@users.noreply.github.com>
2021-03-23 13:35:56 +0100

    Add files via upload

---
+++ js-etravel/nav-trigger.js
@@ -0,0 +1,9 @@
+document.querySelector(".full-nav-trigger").onclick = function() {
+      document.querySelector(".full-nav-trigger").classList.toggle("active");
+
+document.querySelector(".full-nav-close").classList.toggle("full-nav-open");
+
+      document.querySelector("body").classList.toggle("full-nav-open");
+
+
+  };
